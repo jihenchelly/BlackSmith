7b203f06a420d709375c416dc9a76af48ea08976
jihenchelly <jihen.chely@esprit.tn>
2019-01-18 18:52:53 +0100

    deuxieme commit

---
+++ src/components/SideMenu/component.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SideMenu.css';
+
+const SideMenu = ({
+  updateHeaderTitle,
+  updateViewType,
+  fetchFeatured,
+  fetchRecentlyPlayed,
+  fetchSongs,
+  fetchAlbums,
+  fetchArtists,
+  token,
+  title,
+  artistIds
+}) => {
+
+
+  const handleClick = (name)  => {
+    updateHeaderTitle(name);
+    updateViewType(name);
+  };
+
+
+  const renderSideMenu = () => {
+    const menu = [
+      {
+        name: 'Artists',
+        action: fetchArtists,
+        getArtists: true
+      }
+    ];
+
+    return menu.map(item => {
+      return (
+        <li key={ item.name }
+          className={title === item.name ? 'active side-menu-item': 'side-menu-item'}
+          onClick={() => {
+            item.getArtists ? item.action(token, artistIds) : item.action(token);
+            handleClick(item.name); }
+          }>
+          { item.name }
+        </li>
+      );
+    });
+  };
+
+  return (
+    <ul className='side-menu-container'>
+ 
+      {
+        renderSideMenu()
+      }
+    </ul>
+  );
+
+};
+
+SideMenu.propTypes = {
+  updateHeaderTitle: PropTypes.func,
+  updateViewType: PropTypes.func,
+  fetchFeatured: PropTypes.func,
+  fetchRecentlyPlayed: PropTypes.func,
+  fetchSongs: PropTypes.func,
+  fetchAlbums: PropTypes.func,
+  fetchArtists: PropTypes.func,
+  token: PropTypes.string,
+  artistIds: PropTypes.string,
+  title: PropTypes.string
+};
+
+export default SideMenu;
---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from "redux";
+import userReducer from './userReducer';
+import tokenReducer from './tokenReducer';
+import playlistReducer from './playlistReducer';
+import songsReducer from './songsReducer';
+import albumsReducer from './albumsReducer';
+import artistsReducer from './artistsReducer';
+import uiReducer from './uiReducer';
+import soundReducer from './soundReducer';
+
+export default combineReducers({
+  userReducer,
+  tokenReducer,
+  playlistReducer,
+  songsReducer,
+  albumsReducer,
+  artistsReducer,
+  uiReducer,
+  soundReducer
+});
